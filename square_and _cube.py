c2b0c1812f42247bfe6f7c0016aa52f0705a213d
RajathRPAI <117572868+RajathRPAI@users.noreply.github.com>
2022-11-19 14:43:33 +0530

    Update square_and _cube.py

--- square_and _cube.py
+++ square_and _cube.py
@@ -1,13 +1,13 @@
-num = int(input("Enter the total numbers you want to enter : "))
+num = int(input("GIVE THE INPUT : "))
 cube = 0
 for i in range(num) :
     a = int(input("Enter the number :"))
     cube = cube + a*a*a
     average = cube / num
-print("\n Sum of the cubes of the numbers:",cube)
-print("\n Average of Sum of the cubes of the numbers : ", average)
+print("\n Sum of the CUBE of the numbers:",cube)
+print("\n Average of Sum of the CUBE of the numbers : ", average)
 print()
-new_option = eval(input("Do you want continue other CUBE operation (yes- 1 or no-0): "))
+new_option = eval(input("Do you want continue other SQUARE operation (yes- 1 or no-0): "))
 if new_option == 1:
     num =int(input("Enter the total numbers you want to enter : "))
     square = 0
@@ -15,8 +15,8 @@ if new_option == 1:
         a = int(input("Enter the number : "))
         square = square + a * a
         average = square / num
-    print("\nSum of the cubes of the numbers : ", square)
-    print("\n Average of Sum of the cubes of the numbers : ", average)
+    print("\nSum of the SQUARE of the numbers : ", square)
+    print("\n Average of Sum of the SQUARE of the numbers : ", average)
 else:
-    print("**************** THANK YOY SO MUCH **************** ")
+    print("**************** THANK YOU SO MUCH **************** ")
 
